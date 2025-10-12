502c865f6e4ffa766afc877529650365ffaf40d3
Verone Lazio <verone.lazio@gmail.com>
2025-10-11 22:57:39 -0400

    fix: correct GitHub release URL to hivetechs-collective/Hive repo

--- Casks/hive-consensus.rb
+++ Casks/hive-consensus.rb
@@ -2,7 +2,7 @@ cask "hive-consensus" do
   version "1.8.535"
   sha256 "bad3514b6ef1be0efe478d8c0f2ef51586c6aaac4311183def1fe1d847dce7fa"
 
-  url "https://github.com/hivetechs-collective/homebrew-tap/releases/download/v#{version}/Hive-Consensus-#{version}.dmg"
+  url "https://github.com/hivetechs-collective/Hive/releases/download/v#{version}/Hive-Consensus-#{version}.dmg"
   name "Hive Consensus"
   desc "AI development environment with integrated terminals and consensus"
   homepage "https://github.com/hivetechs-collective/homebrew-tap"
